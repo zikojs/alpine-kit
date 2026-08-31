cd16c5125eca5b2c29e4babe5f8e8e34700f14d0
zakarialaoui10 <zakarialaoui10@gmail.com>
2026-08-31 13:19:30 +0100

    setup alpine kit monorepo

--- demo/src/hello.jsx
+++
@@ -1,5 +0,0 @@
-export default function Hello({name = "world"} = {}, Item){
-    return <div>
-           <h1> Hello {name} </h1>
-           </div>
-}
--- demo/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import ZikoJSX from 'ziko-jsx/vite'
-
-
-export default defineConfig({
-  plugins:[
-    ZikoJSX()
-  ]
-})
